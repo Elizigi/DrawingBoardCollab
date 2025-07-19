1b3959b386c5dfedf7e5928276e7905035b77cbc
Elizigi <elizigi876@gmail.com>
2025-07-19 11:18:49 +0300

    eee

--- DrawingBoard/src/Main.tsx
+++ DrawingBoard/src/Main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./main.css";
-import { Application } from "pixi.js";
+import { Application, Graphics } from "pixi.js";
 
 const rootElement = document.getElementById("root") as HTMLDivElement;
 
@@ -10,9 +10,17 @@ const rootElement = document.getElementById("root") as HTMLDivElement;
   const app = new Application();
   await app.init({
     resizeTo: window,
-    backgroundColor: 0xff000
+    backgroundColor: 0xffffff,
   });
   rootElement.appendChild(app.canvas);
+  const drawing = new Graphics();
+  drawing
+    .circle(150, 150, 50)
+    .fill({ color: 0x00ff00 })
+    .beginPath()
+    .circle(250, 150, 50)
+    .fill({ color: 0x0000ff });
+  app.stage.addChild(drawing);
 })();
 
 if (rootElement) {
