8ef193c03dad3dffec0e90893e6bbd2eaa945288
Elizigi <elizigi876@gmail.com>
2025-08-25 00:29:51 +0300

    updated some ui

--- client/src/components/topRightToolbar/TopRightToolbar.tsx
+++ client/src/components/topRightToolbar/TopRightToolbar.tsx
@@ -2,30 +2,26 @@ import styles from "./TopRightToolbar.module.scss";
 import OnlineComponent from "../onlineComponent/OnlineComponent";
 import TopRightToolbarVM from "./TopRightToolbarVM";
 const TopRightToolbar = () => {
-  const {
-    isOnline,
-    handleOnline,
-    handleMenuOpen,
-    handleAnim,
-    hasInteracted,
-    selfId,
-  } = TopRightToolbarVM();
+  const { isOnline, spinnerStyle, selfId, menuOpen, handleOnline } =
+    TopRightToolbarVM();
 
   return (
     <div className={styles.topRightToolContainer}>
-      <div className={`${styles.subMenu} ${hasInteracted ? handleAnim() : ""}`}>
+      <div className={`${styles.subMenu}`}>
         {isOnline ? (
           <OnlineComponent selfId={selfId} />
         ) : (
-          <button onClick={handleOnline} className={styles.onlineBtn}>
+          <button className={styles.onlineBtn}>
             <span>&#127760; Connect To Server</span>
           </button>
         )}
       </div>
+      {menuOpen && <button>Host</button>}
 
-      <button className={styles.topArrowMenu} onClick={handleMenuOpen}>
-        <h3>∨</h3>
+      <button className={styles.topArrowMenu} onClick={handleOnline}>
+        <span className={spinnerStyle}>🌐</span>
       </button>
+      {menuOpen && <button>Join</button>}
     </div>
   );
 };
--- client/src/components/topRightToolbar/TopRightToolbarVM.tsx
+++ client/src/components/topRightToolbar/TopRightToolbarVM.tsx
@@ -1,34 +1,41 @@
 import { useEffect, useState } from "react";
 import { onlineStatus, socket } from "../../Main";
 import styles from "./TopRightToolbar.module.scss";
-
 const TopRightToolbarVM = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [hasInteracted, setHasInteracted] = useState(false);
   const [isOnline, setIsOnline] = useState(false);
   const [selfId, setSelfId] = useState("");
+  const [spinnerStyle, setSpinnerStyle] = useState("");
+
   const handleMenuOpen = () => {
-    setHasInteracted(true);
     setMenuOpen(!menuOpen);
   };
-  const handleAnim = () => {
-    return menuOpen ? styles.openMenuAnim : styles.closeMenuAnim;
-  };
+
+  useEffect(() => {
+    if (!hasInteracted) return;
+    if (!isOnline) {
+      setSpinnerStyle(styles.spinLoad);
+    } else if (isOnline) setSpinnerStyle(styles.spinFinish);
+  }, [isOnline, hasInteracted]);
+
   const handleOnline = () => {
     socket.connect();
-    onlineStatus.isOnline=true;
+    setHasInteracted(true);
+    onlineStatus.isOnline = true;
   };
 
   useEffect(() => {
     socket.on("disconnect", () => {
       setIsOnline(false);
-      onlineStatus.isOnline=false;
-      onlineStatus.inRoom=false;
+      onlineStatus.isOnline = false;
+      onlineStatus.inRoom = false;
       console.log("Disconnected");
     });
 
     socket.on("connect", () => {
       setIsOnline(true);
+      handleMenuOpen();
       console.log("Connected:", socket.id);
     });
     socket.on("user-id", (userId: string) => {
@@ -41,11 +48,10 @@ const TopRightToolbarVM = () => {
   }, []);
   return {
     isOnline,
-    handleOnline,
-    handleMenuOpen,
-    handleAnim,
-    hasInteracted,
+    spinnerStyle,
     selfId,
+    menuOpen,
+    handleOnline,
   };
 };
 
