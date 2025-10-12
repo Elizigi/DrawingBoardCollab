28cae60e70779e4d45c6233c6c9e8ba490e202ab
Elizigi <elizigi876@gmail.com>
2025-10-12 20:52:31 +0300

    admin can kick users

--- client/src/components/topRightToolbar/userList/UserList.tsx
+++ client/src/components/topRightToolbar/userList/UserList.tsx
@@ -1,27 +1,36 @@
 import { FC } from "react";
 import { ConnectedUser } from "../TopRightToolbarVM";
 import styles from "./UserList.module.scss";
+import { onlineStatus, socket } from "../../../Main";
 interface UserListProps {
   connectedUsers: ConnectedUser[];
   menuOpen: boolean;
 }
 const UserList: FC<UserListProps> = ({ connectedUsers, menuOpen }) => {
+
+  const removeUser=(guestId:string)=>{
+    socket.emit("remove-user",{guestId})
+  }
+
   return (
     <>
       {connectedUsers.length > 0 && (
-        <div className={`${styles.userList} ${menuOpen ? styles.shown : styles.transparent}`}>
-          <h3 style={{color:menuOpen?"black":"transparent"}}>Users in the Room:</h3>
-          <div
-            className={styles.usersContainer}
-          >
+        <div
+          className={`${styles.userList} ${menuOpen ? styles.shown : styles.transparent}`}
+        >
+          <h3 style={{ color: menuOpen ? "black" : "transparent" }}>
+            Users in the Room:
+          </h3>
+          <div className={styles.usersContainer}>
             {connectedUsers.map((user) => (
-              <button className={styles.guestIcon} key={user.guestId}>
+              <div className={styles.guestIcon} key={user.guestId}>
                 <div
                   className={styles.userColorIcon}
                   style={{ backgroundColor: user.color }}
                 ></div>
-                {user.name}
-              </button>
+               <h2> {user.name}</h2>
+                {onlineStatus.isAdmin && menuOpen && <button className={styles.removeUser} onClick={()=>removeUser(user.guestId)}>X</button>}
+              </div>
             ))}
           </div>
         </div>
