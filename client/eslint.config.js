ad4a5275477351b9e50f0f395fa7489477745561
Elizigi <elizigi876@gmail.com>
2025-11-12 20:03:40 +0200

    upload

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -4,33 +4,41 @@ import tsEslint from "typescript-eslint";
 import prettierConfig from "eslint-config-prettier";
 
 export default [
-  js.configs.recommended,
+    {
+        ignores: [
+            "node_modules",
+            "client/dist/**/*",
+            "server/dist/**/*"
+        ]
+    },
 
-  ...tsEslint.configs.recommended,
+    js.configs.recommended,
+    ...tsEslint.configs.recommended,
 
-  {
-    files: ["**/*.{ts,tsx,js,jsx,mjs,cjs}"],
-    ignores: ['dist', 'node_modules'],
-    
-    languageOptions: {
-      parser: tsEslint.parser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        ecmaFeatures: {
-          jsx: true,
+    {
+        files: ["**/*.{ts,tsx,js,jsx,mjs,cjs}"],
+
+        languageOptions: {
+            parser: tsEslint.parser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
         },
-      },
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
+        
+        rules: {
+            'semi': ['error', 'never'],
+            'no-prototype-builtins': 'off',
+            '@typescript-eslint/no-unused-expressions': 'off',
+        },
     },
     
-    rules: {
-      'semi': ['error', 'never'], 
-    },
-  },
-  
-  prettierConfig,
+    prettierConfig,
 ];
